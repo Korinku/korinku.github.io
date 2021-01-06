aff3cc95ae2856c42cd848b5daf4d0e52751cd7d
André Silva <8180389@estg.ipp.pt>
2021-01-06 14:53:54 +0000

    Updates

---
+++ precache-manifest.e50c3286424d7106c147a173c85c96f1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d88f13ff0f0a2fd5bc492f53b9acb1e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c2e7794ddaea7bccd616",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cbeed7dbec3c3d9a8239",
+    "url": "/portfolio/static/css/main.8ec59f61.chunk.css"
+  },
+  {
+    "revision": "c2e7794ddaea7bccd616",
+    "url": "/portfolio/static/js/2.124d37e0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.124d37e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbeed7dbec3c3d9a8239",
+    "url": "/portfolio/static/js/main.4e0c418c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "1fe4429affef76a2240faecb87dc6c55",
+    "url": "/portfolio/static/media/FolderVintageWatches.1fe4429a.svg"
+  },
+  {
+    "revision": "2877364ad98275d1079aa7e2d827c711",
+    "url": "/portfolio/static/media/avatar.2877364a.png"
+  },
+  {
+    "revision": "baf1b68a3e8bfe9c5efbb1c3e03c323b",
+    "url": "/portfolio/static/media/logo.baf1b68a.svg"
+  }
+]);
