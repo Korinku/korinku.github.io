6b7f1a9195f855b88a905ce41116f2e722e4e101
André Silva <8180389@estg.ipp.pt>
2021-01-06 16:10:44 +0000

    Updates

---
+++ precache-manifest.baf04fded5aa294c617ac2559a9e2e34.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0734af11d6682882b436397477397884",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2e7794ddaea7bccd616",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cb639165bfeaa61631b6",
+    "url": "/static/css/main.8ec59f61.chunk.css"
+  },
+  {
+    "revision": "c2e7794ddaea7bccd616",
+    "url": "/static/js/2.124d37e0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.124d37e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb639165bfeaa61631b6",
+    "url": "/static/js/main.871c9846.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "1fe4429affef76a2240faecb87dc6c55",
+    "url": "/static/media/FolderVintageWatches.1fe4429a.svg"
+  },
+  {
+    "revision": "2877364ad98275d1079aa7e2d827c711",
+    "url": "/static/media/avatar.2877364a.png"
+  },
+  {
+    "revision": "baf1b68a3e8bfe9c5efbb1c3e03c323b",
+    "url": "/static/media/logo.baf1b68a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e435556d769a591fc7fb26810050d8bb.js"
+  "/precache-manifest.baf04fded5aa294c617ac2559a9e2e34.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
